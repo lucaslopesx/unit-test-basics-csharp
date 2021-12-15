9bdcacd699c34973731d8b5690bbb1ae4603d79f
Lucas Lopes <lucas.lopesx1@gmail.com>
2021-12-15 11:43:58 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# unit-test-basics-csharp
+ 
---
+++ unit-test-basics-csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace unit_test_basics_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
